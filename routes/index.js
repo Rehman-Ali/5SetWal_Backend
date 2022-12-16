cb294715c43ef7dd2036697ee1a2344676e48dcc
Rehman Ali <rehmanali1477@gmail.com>
2022-12-16 20:25:13 +0500

    Fix the issue of Material-Table

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => res.render("signin", {
-    email: '',
-    password: ''        
-}));
+router.get("/", (req, res) => res.json({message: "welcome to node.js"}));
 module.exports = router;
