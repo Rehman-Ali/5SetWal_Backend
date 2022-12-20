2f4af92008cfde983e0d62ac2d05d0094730288d
sssghulamrasool <seositesoft7@gmail.com>
2022-12-20 12:10:37 +0500

    updated

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,27 +1,22 @@
 var exports = (module.exports = {});
-const sequelize = require('sequelize')
+const sequelize = require("sequelize");
 const Model = require("../models");
 const wp_posts = Model.wp_posts;
 const Op = sequelize.Op;
 
-
-
-
-
-
 // display all Post
 exports.getAllPost = async (req, res, next) => {
   try {
-    console.log("working here")
+    console.log("working here");
     let data = await wp_posts.findAll({
       where: {
-        post_type:'post'
-      }
+        post_type: "post",
+      },
     });
     if (data.length < 0) {
       res.json({
         message: "No data found!",
-        success: 0
+        success: 0,
       });
     } else {
       res.status(200).json({
@@ -34,41 +29,32 @@ exports.getAllPost = async (req, res, next) => {
     res.status(500).json({
       data: [],
       message: "Server Internal Error.",
-      success: 0
-    })
+      success: 0,
+    });
   }
-
 };
-
-
 
 // update single Post Data
 exports.updateSinglePost = async (req, res, next) => {
-
-
   await wp_posts.update(req.body, {
     where: {
-      ID: req.params.id
-    }
+      ID: req.params.id,
+    },
   });
   res.status(200).json({
     message: "Post updated succesfully",
-    success: 1
+    success: 1,
   });
-
 };
 
-
-
-
-// display single Post data 
+// display single Post data
 exports.getSinglePost = async (req, res, next) => {
   try {
     let id = req.params.id;
     let data = await wp_posts.findOne({
       where: {
-        ID: id
-      }
+        ID: id,
+      },
     });
 
     if (data === null || data == undefined) {
@@ -78,29 +64,22 @@ exports.getSinglePost = async (req, res, next) => {
         success: 1,
       });
     } else {
-
       res.status(200).json({
         data: data,
         message: "Post get Successfully.",
         success: 1,
       });
     }
-
   } catch (error) {
     res.status(500).json({
       data: [],
       message: "Server Internal Error.",
-      success: 0
-    })
-
+      success: 0,
+    });
   }
-
 };
 
-
-
-
-// Delete Post 
+// Delete Post
 exports.deletePost = async (req, res, next) => {
   try {
     let id = req.params.id;
@@ -113,8 +92,8 @@ exports.deletePost = async (req, res, next) => {
     } else {
       await wp_posts.destroy({
         where: {
-          ID: id
-        }
+          ID: id,
+        },
       });
       return res.status(200).json({
         message: "post deleted successfully.",
@@ -124,9 +103,7 @@ exports.deletePost = async (req, res, next) => {
   } catch (error) {
     res.status(500).json({
       message: "Server Internal Error.",
-      success: 0
-    })
+      success: 0,
+    });
   }
 };
-
-
